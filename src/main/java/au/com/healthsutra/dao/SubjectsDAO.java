67a0baf07a11c72832a03a10d9d76b921adfc71f
Piyush Ramavat <piyu.aryan@gmail.com>
2015-07-19 14:55:37 +1000

    Fixed MVC structural changes. web-mvc to scan controllers only and appContext to scan other components.

--- src/main/java/au/com/healthsutra/dao/SubjectsDAO.java
+++ src/main/java/au/com/healthsutra/dao/SubjectsDAO.java
@@ -5,5 +5,5 @@ import au.com.healthsutra.model.Subjects;
 import java.util.List;
 
 public interface SubjectsDAO {
-    public List<Subjects> findAll();
+    public List findAll();
 }
--- src/main/java/au/com/healthsutra/dao/SubjectsDAOImpl.java
+++ src/main/java/au/com/healthsutra/dao/SubjectsDAOImpl.java
@@ -2,24 +2,17 @@ package au.com.healthsutra.dao;
 
 import au.com.healthsutra.model.Subjects;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
-public class SubjectsDAOImpl extends AbstractDAO implements SubjectsDAO{
+public class SubjectsDAOImpl extends AbstractDAO implements SubjectsDAO {
 
-    /**
-     * @Transactional annotation below will trigger Spring Hibernate transaction manager to automatically create
-     * a hibernate session. See src/main/webapp/WEB-INF/servlet-context.xml
-     */
     @Transactional
     public List<Subjects> findAll() {
         Session session = getSession();
-        List subjects = session.createQuery("from Subjects").list();
-        return subjects;
+        return session.createQuery("from Subjects").list();
     }
 }
